8449cbcc5fc14a8c48a91a74f3fe47b3157ee6ba
BryceHamilton <bryce.m.hamilton@outlook.com>
2021-04-03 18:29:58 -0400

    remove cors

--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ const options = {
   useUnifiedTopology: true,
 };
 express()
-  .use(cors({ origin: 'https://mern-client.netlify.app' }))
   .use(morgan('dev'))
   .use(express.urlencoded({ extended: false }))
   .use(express.json())
