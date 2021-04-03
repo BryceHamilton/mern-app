f58a37ff380d0170ad6ca404ef51621925e14b13
BryceHamilton <bryce.m.hamilton@outlook.com>
2021-04-03 18:31:24 -0400

    revert changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  useEffect(() => {
-    const url = 'https://my-mern-app-demo.herokuapp.com/greeting';
-    fetch(url)
-      .then((res) => res.json())
-      .then((json) => {
-        setGreeting(json.greeting);
-      });
-  }, []);
   return (
     <div className='App'>
-      <header className='App-header'>{greeting}</header>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
